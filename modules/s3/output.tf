0dbe471d7abf40c10e1837de6adbb6f570c05c2e
Lee Coupe <lee.coupe@immediate.co.uk>
2020-02-04 14:22:21 +0000

    revert s3 output change

--- modules/s3/output.tf
+++ modules/s3/output.tf
@@ -1,9 +1,9 @@
 output "s3_source_bucket_id" {
-  value = aws_s3_bucket.function_codepipeline_source_packages.*.id
+  value = aws_s3_bucket.function_codepipeline_source_packages.id
 }
 
 output "s3_source_bucket_arn" {
-  value = aws_s3_bucket.function_codepipeline_source_packages.*.arn
+  value = aws_s3_bucket.function_codepipeline_source_packages.arn
 }
 
 output "s3_cache_bucket_id" {
