1f1f5001abd539a5171f95c0d789f13c713f818b
Lee Coupe <lee.coupe@immediate.co.uk>
2020-02-06 21:16:11 +0000

    removed module level providers

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,6 +1,5 @@
 # IAM Role
 resource "aws_iam_role" "remote_codebuild_role" {
-  provider = aws.remote_account
   name               = "${var.function_prefix}-${var.environment}-remote-codebuild-role"
   assume_role_policy = templatefile("${path.module}/codebuild-remote-cross-account-template.json", {
     aws_account_number = var.aws_account_number,
@@ -10,7 +9,6 @@ resource "aws_iam_role" "remote_codebuild_role" {
 
 # IAM polices
 resource "aws_iam_role_policy" "remote_codebuild_policy" {
-  provider = aws.remote_account
   name   = "${var.function_prefix}-${var.environment}-codebuild-serverless-policy"
   role   = aws_iam_role.remote_codebuild_role.id
   policy = file("${path.module}/serverless-role-policy-template.json")
