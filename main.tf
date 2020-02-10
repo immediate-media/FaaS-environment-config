ed4e95a22817f219a90956ebcf8c74f33db35280
Ben Townsend <38033999+im-btownsend@users.noreply.github.com>
2020-02-10 17:50:55 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,12 +9,6 @@ provider "aws" {
 
 module "s3" {
   source                       = "./modules/s3"
-  function_name                = var.function_name
-  function_prefix              = var.function_prefix
-  platform                     = var.platform
-  environment                  = var.environment
-  region                       = var.region
-  lambda_package               = var.lambda_package
 }
 
 ##############
@@ -23,25 +17,6 @@ module "s3" {
 
 module "lambda" {
   source                       = "./modules/lambda"
-  function_name                = var.function_name
-  function_prefix              = var.function_prefix
-  platform                     = var.platform
-  region                       = var.region
-  environment                  = var.environment
-
-  lambda_runtime               = var.lambda_runtime
-  lambda_package               = "${var.lambda_package != "" ? var.lambda_package : module.s3.s3_dummy_package}"
-  lambda_handler               = var.lambda_handler
-  lambda_memory                = var.lambda_memory
-  lambda_timeout               = var.lambda_timeout
-  lambda_vpc_subnets           = var.lambda_vpc_subnets
-  lambda_security_groups       = var.lambda_security_groups
-  lambda_environment_variables = var.lambda_environment_variables
-
-  log_retention                = var.log_retention
-
-  s3_bucket_name               = module.s3.s3_bucket_name
-  source_arn                   = "${module.api_gateway.execution_arn}/*/*/*"
 }
 
 ###################
@@ -50,12 +25,4 @@ module "lambda" {
 
 module "api_gateway" {
   source                       = "./modules/api-gateway"
-  function_name                = var.function_name
-  function_prefix              = var.function_prefix
-  platform                     = var.platform
-  region                       = var.region
-  environment                  = var.environment
-
-  lambda_invoke_uri            = module.lambda.lambda_invoke_uri
-  api_gateway_content_handling = var.api_gateway_content_handling
 }
