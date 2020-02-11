920541c86ce8e28eb475acc9cc4489e91d94b846
Ben Townsend <38033999+im-btownsend@users.noreply.github.com>
2020-02-11 09:15:55 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,3 @@ provider "aws" {
   version = "~> 2.22"
   region  = var.region
 }
-
-##############
-### Lambda ###
-##############
-
-module "lambda" {
-  source                       = "./modules/lambda"
-  region  = var.region
-}
