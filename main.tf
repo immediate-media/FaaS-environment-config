820129c8d8c21ee7647c768123a282823aae3710
Ben Townsend <38033999+im-btownsend@users.noreply.github.com>
2020-02-11 09:15:03 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,29 +3,11 @@ provider "aws" {
   region  = var.region
 }
 
-##########
-### S3 ###
-##########
-
-module "s3" {
-  source                       = "./modules/s3"
-  region  = var.region
-}
-
 ##############
 ### Lambda ###
 ##############
 
 module "lambda" {
   source                       = "./modules/lambda"
-  region  = var.region
-}
-
-###################
-### API Gateway ###
-###################
-
-module "api_gateway" {
-  source                       = "./modules/api-gateway"
   region  = var.region
 }
