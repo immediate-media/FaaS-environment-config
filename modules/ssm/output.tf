80b5f2d9261d6b4707595658fb7287c2554df74f
Ricardo Barros <ricardofbarros@hotmail.com>
2020-02-24 14:35:06 +0000

    fix: SSM ARN output

--- modules/ssm/output.tf
+++ modules/ssm/output.tf
@@ -1,3 +1,7 @@
 output "kms_key_arn" {
   value = length(aws_kms_key.kms_key) > 0 ? aws_kms_key.kms_key[0].arn : ""
 }
+
+output "ssm_key_arn" {
+  value = length(aws_kms_key.kms_key) > 0 ? aws_ssm_parameter.ssm_ps_prod[0].arn : ""
+}
