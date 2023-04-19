1a40c0ee27e688bad16b4cc88921b3d04d866c4d
im-jvickers <107053170+im-jvickers@users.noreply.github.com>
2023-04-19 14:25:39 +0100

    Update main.tf

--- modules/codepipeline/main.tf
+++ modules/codepipeline/main.tf
@@ -85,7 +85,7 @@ resource "aws_codepipeline" "codepipeline_project" {
       output_artifacts = ["source_output"]
 
       configuration = {
-        owner      = "immediate-media"
+        Owner      = "immediate-media"
         Repo       = var.github_repo
         Branch     = var.github_branch
         OAuthToken = var.github_auth_token
