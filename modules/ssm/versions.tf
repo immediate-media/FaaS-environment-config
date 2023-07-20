32f7a71403f07c4518aba64ad971ac496598eb75
Ben Townsend <ben.townsend@immediate.co.uk>
2023-07-20 12:09:55 +0100

    fix: Changed random provider version
    - Changed random provider version to ~>3.1 which is the minimum for arm support
    
    ticket: N/A
    branch: fix/random-provider-arm

--- modules/ssm/versions.tf
+++ modules/ssm/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     random = {
       source  = "hashicorp/random"
-      version = "~> 2.2"
+      version = "~> 3.1"
     }
     aws = {
       source = "hashicorp/aws"
