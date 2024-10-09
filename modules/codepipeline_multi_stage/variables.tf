aa73f3025b667419f4c2ae1ddcc03d25df79fe0d
mohussei <mohamed.hussein@immediate.co.uk>
2024-10-09 14:23:03 +0100

    variables for the fix

--- modules/codepipeline_multi_stage/variables.tf
+++ modules/codepipeline_multi_stage/variables.tf
@@ -94,3 +94,40 @@ variable "disable_integration_II_test" {
   description = "Disable Integration II Test Stage"
   default     = true
 }
+
+variable "codestar_ghec_connection_arn" {
+  description = "GitHub Enterprise Cloud connection arn to trigger codepipeline"
+  default     = null
+}
+
+variable "codestar_connection_arn" {
+  description = "GitHub connection arn to trigger codepipeline"
+  default     = null
+}
+
+variable "github_public_repo" {
+  description = "Public Git repo to look for source code"
+  type        = string
+  default     = ""
+}
+
+variable "github_public_branch" {
+  description = "Public Git branch to look for changes and clone"
+  type        = string
+  default     = "main"
+}
+
+variable "s3_bucket" {
+  description = "S3 bucket where to store assets"
+}
+
+variable "s3_object_key" {
+  description = "filename of the target zip file for codepipeline."
+  default     = ""
+}
+
+variable "source_provider" {
+  description = "The provider of the service being called by the action. Valid providers are determined by the action category."
+  type        = string
+  default     = "S3"
+}
