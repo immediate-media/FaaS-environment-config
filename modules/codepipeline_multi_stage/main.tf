14d859d15baf787b76f0f9a43f04f290e8001391
Jordan Walster <jordanwalster@googlemail.com>
2025-03-18 13:50:06 +0000

    removed piepline webhook

--- modules/codepipeline_multi_stage/main.tf
+++ modules/codepipeline_multi_stage/main.tf
@@ -3,31 +3,6 @@ provider "github" {
   owner = "immediate-media"
 }
 
-# CodePipeline Webhook
-resource "aws_codepipeline_webhook" "codepipeline_webhook" {
-  name            = "${var.function_prefix}-codepipeline-webhook"
-  authentication  = "GITHUB_HMAC"
-  target_action   = "Source"
-  target_pipeline = aws_codepipeline.codepipeline_project.name
-
-
-  lifecycle {
-    ignore_changes = [
-      authentication_configuration
-    ]
-  }
-
-  authentication_configuration {
-    secret_token     = var.webhook_secret
-    allowed_ip_range = var.webhook_ip_range
-  }
-
-  filter {
-    json_path    = "$.ref"
-    match_equals = "refs/heads/${var.github_branch}"
-  }
-}
-
 # GitHub Webhook
 resource "github_repository_webhook" "github_webhook" {
   repository = var.github_repo
