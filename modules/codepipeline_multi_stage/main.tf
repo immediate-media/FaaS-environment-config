00e087d37f20dd2193548a9bb9402e5900b5e7c6
im-skingswell <46105568+im-skingswell@users.noreply.github.com>
2025-03-20 09:06:54 +0000

    fix: remove github webhook

--- modules/codepipeline_multi_stage/main.tf
+++ modules/codepipeline_multi_stage/main.tf
@@ -2,17 +2,3 @@ provider "github" {
   token = var.github_auth_token
   owner = "immediate-media"
 }
-
-# GitHub Webhook
-resource "github_repository_webhook" "github_webhook" {
-  repository = var.github_repo
-
-  configuration {
-    url          = aws_codepipeline_webhook.codepipeline_webhook.url
-    content_type = "form"
-    insecure_ssl = true
-    secret       = var.webhook_secret
-  }
-
-  events = ["push"]
-}
